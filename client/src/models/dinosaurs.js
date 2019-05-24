afb1dbcd13f43e6525f842654883611ae941ecbe
Andrew Blair <blair.d.andrew@gmail.com>
2019-05-24 14:18:18 +0100

    a bindEvents and getData added

--- client/src/models/dinosaurs.js
+++ client/src/models/dinosaurs.js
@@ -1,16 +1,30 @@
-const Request = require('../helpers/request_helper.js');
+const RequestHelper = require('../helpers/request_helper.js');
 const PubSub = require('../helpers/pub_sub.js');
 
 const Dinosaurs = function(url) {
     this.url = url
 }
 
+
+
 Dinosaurs.prototype.bindEvents = function() {
-    PubSub.subscribe('Dinosaurs:data_ready', event => {
-        this.getGame(event.detail)
-    })
+    PubSub.publish('SelectView:Dinosaurs-diet-Change', event => {
+        const dietIndex = event.detail()
+        this.filteredDietDiosaurs(dietIndex);
+
+})
 }
 
 Dinosaurs.prototype.getData = function() {
-    
+    const request = new RequestHelper(this.url);
+    request.get()
+    .then((dinosaurs) => {
+        PubSub.publish('Dinosaurs:data-loaded', dinosaurs);
+        this.publishDietTypes(dinosaurs)
+    })
+    .catch(console.error);
 }
+
+
+
+module.exports = Dinosaurs
