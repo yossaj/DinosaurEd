a2477b0216cd929033667e64dfaf62bd252955e2
Andrew Blair <blair.d.andrew@gmail.com>
2019-05-24 15:15:10 +0100

    unique diet list function

--- client/src/models/dinosaurs.js
+++ client/src/models/dinosaurs.js
@@ -4,27 +4,30 @@ const PubSub = require('../helpers/pub_sub.js');
 const Dinosaurs = function(url) {
     this.url = url
 }
-
-
-
 Dinosaurs.prototype.bindEvents = function() {
     PubSub.subscribe('SelectView:Dinosaurs-diet-Change', event => {
         const dietIndex = event.detail()
         this.filteredDietDiosaurs(dietIndex);
-
 })
 }
-
 Dinosaurs.prototype.getData = function() {
     const request = new RequestHelper(this.url);
     request.get()
     .then((dinosaurs) => {
+        this.dinoData = dinosaurs;
         PubSub.publish('Dinosaurs:data-loaded', dinosaurs);
         this.publishDietTypes(dinosaurs)
     })
     .catch(console.error);
 }
-
-
+Dinosaurs.prototype.uniqueDietList = function () {
+    return this.dietList().filter((diet, index, array) => {
+        return array.indexOf(diet) === index;
+    })
+}
+Dinosaurs.prototype.dietListFunction = function() {
+    const dietList = this.dinoData.map(dinosaur => dinosaur.diet);
+    return dietList;
+}
 
 module.exports = Dinosaurs
