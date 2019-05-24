7a14c99948d6ce9c97a167d69138b61684b20e82
yossaj <yossaj@icloud.com>
2019-05-24 14:33:10 +0100

    url added to apps

--- client/src/app.js
+++ client/src/app.js
@@ -1,22 +1,25 @@
-const DinoSelect = require('/viewsdino_select_view.js')
-const DinoGrid = require('./views/dino_grid_view.js')
-const DinoGridList = require('./views/dino_grid_item_view.js')
+// const DinoSelect = require('/viewsdino_select_view.js')
+// const DinoGrid = require('./views/dino_grid_view.js')
+// const DinoGridList = require('./views/dino_grid_item_view.js')
 // const DinoDetail = require('./views/dino_detail_view.js')
 const Dinosaurs = require('./models/dinosaurs.js')
 
 
 document.addEventListener('DOMContentLoaded', ()=>{
+    console.log('javascript loaded');
+    
 
-    const dinoContainer = document.querySelector('select#diet')
-    const dinoSelect = new DinoSelect(dinoContainer)
-    dinoSelect.bindEvents();
+    // const dinoContainer = document.querySelector('select#diet')
+    // const dinoSelect = new DinoSelect(dinoContainer)
+    // dinoSelect.bindEvents();
 
-    const dinoGrid = new DinoGrid();
-    dinoGrid.bindEvents();
+    // const dinoGrid = new DinoGrid();
+    // dinoGrid.bindEvents();
 
-    const dinoGridList = new dinoGridList();
-    dinoGridList.bindEvents();
+    // const dinoGridList = new dinoGridList();
+    // dinoGridList.bindEvents();
 
-    const dinosaur = new Dinosaur();
-    dinosaurs.getData();
+    const url = 'http://localhost:3000/api/dinosaurs'
+    const dinosaur = new Dinosaurs(url);
+    dinosaur.getData();
 })
