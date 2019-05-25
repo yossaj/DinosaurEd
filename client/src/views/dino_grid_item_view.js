507c8f1e305f6ad84d3a801079733dff026aa92b
yossaj <yossaj@icloud.com>
2019-05-25 12:04:35 +0100

    added to the select

--- client/src/views/dino_grid_item_view.js
+++ client/src/views/dino_grid_item_view.js
@@ -12,12 +12,13 @@ DinoGridItem.prototype.render = function(){
     for(const dinosaur of this.dinosaurs){
         const listContainer = document.createElement('div')
         listContainer.classList.add('dino-box')
-        const name = document.createElement('h2')
+        const name = document.createElement('p')
         name.textContent = dinosaur.name
         const image = document.createElement('img')
+        image.src = dinosaur.images.dinoImage
         this.container.appendChild(listContainer)
-        listContainer.appendChild(name)
         listContainer.appendChild(image)
+        listContainer.appendChild(name)
     }
 }
 
