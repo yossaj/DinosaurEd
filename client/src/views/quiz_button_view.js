414f290894e5180f963fa2a3836a041da371f6e6
Alison Williams <alison.lauriewill@gmail.com>
2019-05-28 09:22:08 +0100

    created quiz_button_view.js, button element to HTML and required view in app.js

---
+++ client/src/views/quiz_button_view.js
@@ -0,0 +1,15 @@
+const PubSub = require('../helpers/pub_sub.js')
+
+const QuizButtonView = function (container) {
+  this.container = container
+}
+
+
+QuizButtonView.prototype.bindEvents = function () {
+
+};
+
+
+
+
+module.exports = QuizButtonView;
